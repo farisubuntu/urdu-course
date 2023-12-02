afbfdb5ee518ec82fa0a96979d99b7554bf6bbd4
farisubuntu <farisubuntu@gmail.com>
2023-12-02 21:00:42 +0300

    add next dir

---
+++ next/data.js
@@ -0,0 +1,93 @@
+// data.js => only variables
+
+
+var main_and_mujhe=`
+<button type=button class="collapsible">What is the difference between <em class="ur">مجھے</em> and <em class="ur">میں</em> in Hindi?
+</button>
+<div class="content">
+ <p class="box">
+  <em class=ur>میں</em> is subject case only, <em class=ur>مجھے</em> is for object cases, and it comes in three versions for different levels of language (style):
+  <em class=ur>مجھے</em> (literary and educated), <em class=ur>مجھ کو</em> (everyday usage), and <em class=ur>میرے کو</em> (substandard, but also widely used)
+ </p>
+ <p class="box"> <em class=ur>مجھے</em> and <em class=ur>میں</em> both refer to the self, but they are used in different contexts. <em class=ur>مجھے</em> is used to express that something is happening to or for the speaker, while <em class=ur>میں</em> is used to refer to the speaker as the subject of a sentence. For example, <em class=ur>مجھے khana pasand hai</em> means I like food, while <em class=ur>میں khana kha raha hoon</em> means I am eating food.</p>
+</div>
+
+`;
+
+var urdu_pronouns=`
+<button type="button" class="collapsible">
+  Urdu/Pronouns
+</button>
+<div id="urdu-pronouns" class="content">
+  <table class="wikitable">
+    <tbody>
+      <tr align="center">
+        <th class="empty"></th>
+        <th colspan="2">First Person</th>
+        <th colspan="3">Second Person</th>
+        <th colspan="4">Third Person</th>
+      </tr>
+      <tr align="center">
+        <td class="empty"></td>
+        <td>Singular (I)</td>
+        <td>Plural (We)</td>
+        <td>Formal (You)</td>
+        <td>Informal (You)</td>
+        <td>Very Informal (Thou)</td>
+        <td>Singular (This)</td>
+        <td>Plural (These)</td>
+        <td>Singular (That)</td>
+        <td>Plural (Those)</td>
+      </tr>
+      <tr align="center">
+        <td class="h-cell"><b>Direct</b></td>
+        <td><em>میں</em> / <i>maiⁿ</i></td>
+        <td><em>ہم</em> / <i>ham</i></td>
+        <td><em>آپ</em> / <i>aap</i></td>
+        <td><em>تم</em> / <i>tum</i></td>
+        <td><em>تو</em> / <i>tū</i></td>
+        <td><i>yeh/ye</i><br /><em>یہ</em> / <i>yeh</i></td>
+        <td><i>ye</i><br /><em>یہ</em> / <i>yeh</i></td>
+        <td><i>voh</i><br /><em>وہ</em> / <i>woh</i></td>
+        <td><i>we/wo</i><br /><em>وہ</em> / <i>woh</i></td>
+      </tr>
+      <tr align="center">
+        <td class="h-cell"><b>Accusative</b></td>
+        <td><em>مجھے</em> / <i>mujhey</i></td>
+        <td><em>ہمیں</em> / <i>hamaiⁿ</i></td>
+        <td><em>آپکو</em> / <i>aapko</i></td>
+        <td><em>تمہیں</em> / <i>tumhaiⁿ</i></td>
+        <td><em>تجھے</em> / <i>tujhey</i></td>
+        <td><em>اِسے</em> / <i>isey</i></td>
+        <td><em>اِنہیں</em> / <i>inhaiⁿ</i></td>
+        <td><em>اُسے</em> / <i>usey</i></td>
+        <td><em>اُنہیں</em> / <i>unhaiⁿ</i></td>
+      </tr>
+      <tr align="center">
+        <td class="h-cell"><b>Oblique</b></td>
+        <td><em> مجھ </em> <i>mujh-</i></td>
+        <td><em>ہم</em> / <i>ham-</i></td>
+        <td><em>-آپ</em> / <i>aap-</i></td>
+        <td><em>تم</em> / <i>tum-</i></td>
+        <td><em>تجھ</em> / <i>tujh-</i></td>
+        <td><em>اِس</em> / <i>is-</i></td>
+        <td><em>اِن</em> / <i>in-</i></td>
+        <td><em>اُس</em> / <i>us-</i></td>
+        <td><em>اُن</em> / <i>un-</i></td>
+      </tr>
+      <tr align="center">
+        <td class="h-cell"><b>Genitive</b></td>
+        <td><em> میرا</em> / <i>merā</i></td>
+        <td><em> ہمارا</em> / <i>hamārā</i></td>
+        <td><em> آپکا</em> / <i>aapkā</i></td>
+        <td><em> تمہارا</em> / <i>tumhārā</i></td>
+        <td><em> تیرا</em> / <i>terā</i></td>
+        <td><em> اِسکا</em> / <i>iskā</i></td>
+        <td><em> اِنکا</em> / <i>inkā</i></td>
+        <td><em> اُسکا</em> / <i>uskā</i></td>
+        <td><em> اُنکا</em> / <i>unkā</i></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+`;
