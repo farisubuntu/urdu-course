93dec7b936e9ad2707b0a4884f3e46be5483d89b
farisubuntu <farisubuntu@gmail.com>
2023-12-09 00:15:36 +0300

    fixing no alts prop

--- templates.js
+++ templates.js
@@ -40,57 +40,14 @@ function get_top_bar() {
  `;
     document.querySelector(".nav-header").innerHTML = html;
 }
-
-function get_word_template_table() {
-    console.log("get_word_template_table()....");
-    console.log('lesson: ', lesson);
-    console.log('and quizzes object contains: ', quizzes);
-    var const_audio_portion = "https://d13tz37rv54ob.cloudfront.net/ur/";
-    var const_image_portion =
-        "https://d37sy4vufic209.cloudfront.net/phrase-images/";
-    var totalwords = 0;
-    if (quizzes[0].alts.length) {
-        totalwords = quizzes[0].alts.length;
-    }
-    console.log('totoalwords = ', totalwords);
-
-    var html = `
-<div class="tg-wrap">
-<table>
-<thead>
-  <tr>
-    <th class="target-head">الكلمة</th>
-    <th class="mother-head">المعنى</th>
-    <th class="audio-head">النطق</th>
-    <th class="image-head">صورة</th>
-  </tr>
-</thead>
-<tbody>`;
-    for (var i = 0; i < totalwords; i++) {
-        html += `
-  <tr>
-    <td class="target">${quizzes[0].alts[i].text}</td>
-    <td class="mother">${quizzes[0].sols[i].text}</td>
-    <td class="audio"><i class="fa fa-play"></i><audio preload="metadata"  src='${const_audio_portion +
-    quizzes[0].alts[i].key +
-    "?t=" +
-    quizzes[0].alts[i].audio_updated_at}' 
-      type='audio/mp3'>Your browser not supported</audio></td>
-    <td class="image"><img src='${const_image_portion +
-    quizzes[0].alts[i].image}' /></td>
-  </tr>  
-  `;
-    }
-    html += `
-</tbody>
-</table>
-</div>
-`;
-    console.log('finish filling words table....');
-    document.querySelector(".content-wrapper").innerHTML = html;
-
+//  Global Constant:
+var const_audio_portion = "https://d13tz37rv54ob.cloudfront.net/ur/";
+var const_image_portion = "https://d37sy4vufic209.cloudfront.net/phrase-images/";
+// appendPhrasesData()
+function appendPhrasesData() {
     //-------------- append phrases/examples data: --------
     // var phrases_index = [1, 3, 4, 5, 7, 8];
+    console.log('appendPhrasesData()....');
     var phrases = ``;
     var row = ``;
     for (var i = 0; i < quizzes.length; i++) {
@@ -121,10 +78,61 @@ function get_word_template_table() {
     // append to 'div.quizzes' element
 
     document.querySelector(".quizzes").innerHTML = phrases;
+}
+
+function appendWordsData(totalwords) {
+    console.log('appendWordsData()....');
+    // clean previous content
+    document.querySelector(".content-wrapper").innerHTML = "";
+
+    var html = `
+<div class="tg-wrap">
+<table>
+<thead>
+<tr>
+<th class="target-head">الكلمة</th>
+<th class="mother-head">المعنى</th>
+<th class="audio-head">النطق</th>
+<th class="image-head">صورة</th>
+</tr>
+</thead>
+<tbody>`;
+    for (var i = 0; i < totalwords; i++) {
+        html += `
+<tr>
+<td class="target">${quizzes[0].alts[i].text}</td>
+<td class="mother">${quizzes[0].sols[i].text}</td>
+<td class="audio"><i class="fa fa-play"></i><audio preload="metadata"  src='${const_audio_portion +
+      quizzes[0].alts[i].key +
+      "?t=" +
+      quizzes[0].alts[i].audio_updated_at}' 
+  type='audio/mp3'>Your browser not supported</audio></td>
+<td class="image"><img src='${const_image_portion +
+      quizzes[0].alts[i].image}' /></td>
+</tr>  
+`;
+    }
+    html += `
+</tbody>
+</table>
+</div>
+`;
+    console.log('finish filling words table....');
+    document.querySelector(".content-wrapper").innerHTML = html;
+}
+
+function get_word_template_table() {
+    console.log("get_word_template_table()....");
+    console.log('lesson: ', lesson);
+    console.log('and quizzes object contains: ', quizzes);
+    if ('alts' in quizzes[0]) {
+        var totalwords = quizzes[0].alts.length;
+        appendWordsData(totalwords);
+    }
+    appendPhrasesData();
 
     // add event listner to all fa-play icons to play next audio
     //+ element
-
     document.querySelectorAll("td.audio,.cell.audio").forEach(function f(elem) {
         elem.addEventListener("click", playAudio);
     });
